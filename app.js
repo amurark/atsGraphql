e24e41af9a399efb5e0d944981061fc5a1cab074
Ankit <ankit.murarka1@ibm.com>
2019-12-07 18:05:58 -0500

    made changes to db url

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use('/graphql', graphQlHttp({
 //     res.send('Hello Jee!!');
 // });
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@atstestcluster-ipozf.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
         .then(() => {
             console.log("Connected to the database. Serving at port 3000...");
             app.listen(3000);
